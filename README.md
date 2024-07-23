f2f3a45131a97f17b7875573ca64c96de8bbe54f
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-07-23 18:10:45 +0300

    Sixth step

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 <a href="https://codeclimate.com/github/AlloKuz/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/d8fc2c7390e46fd8c6dc/maintainability" /></a>
 
 <a href="https://asciinema.org/a/tJjL1jlFmQsMvggNlQiwuLR45" target="_blank"><img src="https://asciinema.org/a/tJjL1jlFmQsMvggNlQiwuLR45.svg" /></a>
+
+<script src="https://asciinema.org/a/reJrEtdI5My0NczHISKWWIqzy.js" id="asciicast-reJrEtdI5My0NczHISKWWIqzy" async="true"></script>
---
+++ brain_games/games/calc.py
@@ -0,0 +1,27 @@
+from random import choice, choices
+from brain_games.flo import even_game
+
+case = "What is the result of the expression?"
+
+sign = choice(["+", "-", "*"])
+
+
+def calculated_express(num_1, num_2, sign):
+    if sign == "+":
+        result = num_1 + num_2
+    elif sign == "-":
+        result = num_1 - num_2
+    elif sign == "*":
+        result = num_1 * num_2
+    return result
+
+
+def get():
+    num1, num2 = choices(range(1, 30), k=2)
+    answer = calculated_express(num1, num2, sign)
+    question = "{} {} {}".format(num1, sign, num2)
+    return question, str(answer)
+
+
+def run_game():
+    even_game(get, case)
