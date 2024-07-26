3d34c933e0e0bc7d3543f4f0223804e0df134b7b
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-07-26 15:02:41 +0300

    fix fixing 5

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,10 +6,12 @@ CASE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime(num):
     index = 1
+    count = 0
     while index <= num // 2:
         if num % index == 0:
-            return False 
-    return True
+            count += 1
+        index += 1 
+    return count == 1
 
 
 def get_question():
